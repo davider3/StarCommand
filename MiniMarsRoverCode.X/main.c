c15e683624d8438fd41714688dbf5517dd354a6a
davider3 <dreinhardt389@gmail.com>
2022-10-19 20:01:00 -0600

    Defined tank turn function.  Added Change of state if Statements

--- MiniMarsRoverCode.X/main.c
+++ MiniMarsRoverCode.X/main.c
@@ -16,17 +16,27 @@
 #define SLOWSTEPS 77
 #define ONEREV 200
 #define ONESEC 1938
+#define WHEELDIAMETER 69.5 //mm
+#define TRACKWIDTH 221 //mm
 
-int steps = 0;
+//GLOBAL VARIABLES
+int OC1Steps = 0;
+int OC2Steps = 0;
+int OC3Steps = 0;
+float turnCoeff = TRACKWIDTH / (1.8 * WHEELDIAMETER);
+int stepsToTake = 0;
+int turnDir = 0;
 
+//FUNCTION PROTOTYPES
 void tankTurn(int dir, int degrees);
 void driveStraight();
 void __attribute__((interrupt, no_auto_psv)) _OC1Interrupt(void);
+void __attribute__((interrupt, no_auto_psv)) _OC2Interrupt(void);
+void __attribute__((interrupt, no_auto_psv)) _OC3Interrupt(void);
 
 int main(void) {
     
     //SET UP STEPPER MOTORS
-    
     //RIGHT
     OC2CON1 = 0;
     OC2CON2 = 0;
@@ -34,6 +44,10 @@ int main(void) {
     OC2CON2bits.SYNCSEL = 0x1F;
     OC2CON2bits.OCTRIG = 0;
     OC2CON1bits.OCM = 0b110;
+    
+    _OC2IP = 4; // Select OCx interrupt priority
+    _OC2IE = 1; // Enable OCx interrupt
+    _OC2IF = 0; // Clear OCx interrupt flag
     
     //LEFT
     OC3CON1 = 0;
@@ -43,9 +57,23 @@ int main(void) {
     OC3CON2bits.OCTRIG = 0;
     OC3CON1bits.OCM = 0b110;
     
+    _OC3IP = 4; // Select OCx interrupt priority
+    _OC3IE = 1; // Enable OCx interrupt
+    _OC3IF = 0; // Clear OCx interrupt flag
+    
     
     //SET UP PARAMETERS FOR STATE MACHINE
-    enum {STRAIGHT, RIGHT, TURNAROUND} state;
+    enum {RIGHT, TURNAROUND, STRAIGHT} state;
+    enum {RIGHT, TURNAROUND} prevState;
+    
+    state = STRAIGHT;
+    prevState = TURNAROUND;
+   
+    //TIMER
+    _TON = 1;       // Turn Timer1 on
+    _TCKPS = 0b01;  // Chose prescaling
+    _TCS = 0;       // Internal clock source (FOSC/2)
+    TMR1 = 0;       // Reset Timer1
     
     while(1){
         switch(state){
@@ -53,15 +81,32 @@ int main(void) {
             case STRAIGHT:
                 
                 
-                
+                if(TMR1 >= 2*ONESEC){
+                    if(prevState == RIGHT){
+                        state = TURNAROUND;
+                    }else if(prevState == TURNAROUND){
+                        state = RIGHT;
+                    }
+                }
                 break;
                 
             case RIGHT:
                 
+                
+                
+                if(OC2Steps >= stepsToTake){
+                    state = STRAIGHT;
+                    prevState = RIGHT;
+                }
                 break;
                 
             case TURNAROUND:
                 
+                
+                if(OC2Steps >= stepsToTake){
+                    state = STRAIGHT;
+                    prevState = TURNAROUND;
+                }
                 break;
         }
     }
@@ -72,5 +117,20 @@ int main(void) {
 void __attribute__((interrupt, no_auto_psv)) _OC1Interrupt(void){
     _OC1IF = 0;
     
-    ++steps;
+    ++OC1Steps;
+}
+void __attribute__((interrupt, no_auto_psv)) _OC2Interrupt(void){
+    _OC2IF = 0;
+    
+    ++OC2Steps;
+}
+void __attribute__((interrupt, no_auto_psv)) _OC3Interrupt(void){
+    _OC3IF = 0;
+    
+    ++OC3Steps;
+}
+void tankTurn(int dir, int degrees){
+    stepsToTake = turnCoeff * degrees;
+    
+    turnDir = dir;
 }
