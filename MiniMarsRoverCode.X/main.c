ec8d27ee9f64ecee6f87b5307c9e0a9de74a256c
davider3 <dreinhardt389@gmail.com>
2022-10-19 19:28:25 -0600

    Started Configuration of PWM pins for Steppers. Start of FSM Layout

--- MiniMarsRoverCode.X/main.c
+++ MiniMarsRoverCode.X/main.c
@@ -14,7 +14,7 @@
 #define REST 0
 #define FASTSTEPS 38
 #define SLOWSTEPS 77
-#define ONEREV 400
+#define ONEREV 200
 #define ONESEC 1938
 
 int steps = 0;
@@ -27,11 +27,43 @@ int main(void) {
     
     //SET UP STEPPER MOTORS
     
+    //RIGHT
+    OC2CON1 = 0;
+    OC2CON2 = 0;
+    OC2CON1bits.OCTSEL = 0b111;
+    OC2CON2bits.SYNCSEL = 0x1F;
+    OC2CON2bits.OCTRIG = 0;
+    OC2CON1bits.OCM = 0b110;
+    
+    //LEFT
+    OC3CON1 = 0;
+    OC3CON2 = 0;
+    OC3CON1bits.OCTSEL = 0b111;
+    OC3CON2bits.SYNCSEL = 0x1F;
+    OC3CON2bits.OCTRIG = 0;
+    OC3CON1bits.OCM = 0b110;
     
     
+    //SET UP PARAMETERS FOR STATE MACHINE
+    enum {STRAIGHT, RIGHT, TURNAROUND} state;
     
     while(1){
-        
+        switch(state){
+            
+            case STRAIGHT:
+                
+                
+                
+                break;
+                
+            case RIGHT:
+                
+                break;
+                
+            case TURNAROUND:
+                
+                break;
+        }
     }
     
     return 0;
