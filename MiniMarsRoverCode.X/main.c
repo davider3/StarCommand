441246883c79d34785f9084652e854e135bc138e
davider3 <dreinhardt389@gmail.com>
2022-11-11 18:40:56 -0700

    Readded Code that had been deleted

--- MiniMarsRoverCode.X/main.c
+++ MiniMarsRoverCode.X/main.c
@@ -7,6 +7,7 @@
 
 
 #include "xc.h"
+#include "setup.h"
 #pragma config FNOSC = LPRC //31 kHz oscillator
 
 #define CW 0
@@ -17,10 +18,14 @@
 #define WHEELDIAMETER 69.5 //mm
 #define TRACKWIDTH 221 //mm
 #define FAST 75 //TODO:TRIAL AND ERROR TO DECIDE THE BEST VALUE FOR SPEED
-#define SORTAFAST 150
+#define SORTAFAST 100
 #define SLOW 260
 #define THRESHOLD 2500
 #define LIMIT 300
+#define FRONTSENSOR !_RB8
+#define OPENSERVO 31
+#define CLOSESERVO 15
+#define SERVOPERIOD 387
 
 //GLOBAL VARIABLES
 int OC1Steps = 0;
@@ -31,20 +36,16 @@ int stepsToTake = 0;
 int lineCount = 0;
 
 //FSM VARIABLES
-enum {STRAIGHT, SLIGHTRIGHT, SLIGHTLEFT, HARDRIGHT, HARDLEFT, SEARCH} lineFollowingState;
+enum {STRAIGHT, SLIGHTRIGHT, SLIGHTLEFT, HARDRIGHT, HARDLEFT} lineFollowingState;
 enum {TASKDETECTIONDEFAULT, TASKDETECTIONBLACK, TASKDETECTIONWHITE} taskDetectionState;
-
+enum {GOSTRAIGHT, WALLDETECTED} canyonState;
 
 //FUNCTION PROTOTYPES
-//SETUP FUNCTIONS
-void setupSteppers();
-void setupQRDs();
-void setupTimer();
 
 //FINITE STATE MACHINES
 void lineFollowingFSM();
 void taskDetectionFSM();
-
+void canyonNavigationFSM();
 //CONTROL FUNCTIONS
 void tankTurn(int degrees, int dir);
 void driveStraight();
@@ -53,25 +54,29 @@ void slightLeft();
 void hardRight();
 void hardLeft();
 void search();
+void debugLED(int onOff);
+void openGate();
+void closeGate();
 void sampleReturn();
-
 //CHECK STATE FUNCTIONS
 int rightQRD(); //RETURNS 1 IF BLACK IS DETECTED
 int midQRD(); //RETURNS 1 IF BLACK IS DETECTED
 int leftQRD(); //RETURNS 1 IF BLACK IS DETECTED
 int taskdetectionQRD(); //RETURNS 1 IF BLACK IS DETECTED
-int samplereturnQRD(); //RETURNS 1 IF BLACK IS DETECTED
-
+int ballQRD();
 //INTERRUPTS
 void __attribute__((interrupt, no_auto_psv)) _OC1Interrupt(void);
 void __attribute__((interrupt, no_auto_psv)) _OC2Interrupt(void);
 void __attribute__((interrupt, no_auto_psv)) _OC3Interrupt(void);
+
+
 
 int main(void) {
     
     setupSteppers();
     setupQRDs();
     setupTimer();
+    setupDistanceSensors();
     
     //SET UP PARAMETERS FOR LINE FOLLOWING STATE MACHINE
     lineFollowingState = STRAIGHT;
@@ -79,6 +84,9 @@ int main(void) {
     
     //SET UP PARAMETERS FOR TASK DETECTION STATE MACHINE
     taskDetectionState = TASKDETECTIONDEFAULT;
+    
+    //SET UP PARAMETERS FOR CANYON NAVIGATION STATE MACHINE
+    canyonState = GOSTRAIGHT;
     
     //SET UP LED FOR DEBUGGING
     _TRISB7 = 0;
@@ -89,11 +97,14 @@ int main(void) {
         
         lineFollowingFSM();
         taskDetectionFSM();
+        //canyonNavigationFSM();
         
     }
     
     return 0;
 }
+
+
 
 //INTERRUPTS
 void __attribute__((interrupt, no_auto_psv)) _OC1Interrupt(void){
@@ -110,99 +121,6 @@ void __attribute__((interrupt, no_auto_psv)) _OC3Interrupt(void){
     _OC3IF = 0;
     
     ++OC3Steps;
-}
-
-//SETUP FUNCTION DEFINITIONS
-void setupSteppers(){
-    //RIGHT, uses OC2, pin 4, RB0
-    OC2CON1 = 0;
-    OC2CON2 = 0;
-    OC2CON1bits.OCTSEL = 0b111;
-    OC2CON2bits.SYNCSEL = 0x1F;
-    OC2CON2bits.OCTRIG = 0;
-    OC2CON1bits.OCM = 0b110;
-    
-    _OC2IP = 4; // Select OC2 interrupt priority
-    _OC2IE = 1; // Enable OC2 interrupt
-    _OC2IF = 0; // Clear OC2 interrupt flag
-    
-    //LEFT, uses OC3, pin 5, RB1
-    OC3CON1 = 0;
-    OC3CON2 = 0;
-    OC3CON1bits.OCTSEL = 0b111;
-    OC3CON2bits.SYNCSEL = 0x1F;
-    OC3CON2bits.OCTRIG = 0;
-    OC3CON1bits.OCM = 0b110;
-    
-    _OC3IP = 4; // Select OC3 interrupt priority
-    _OC3IE = 1; // Enable OC3 interrupt
-    _OC3IF = 0; // Clear OC3 interrupt flag
-    
-    //SET UP DIRECTION PINS
-    _TRISA0 = 0;
-    _ANSA0 = 0; //this is unnecessary
-    _ANSA1 = 0; //this is unnecessary
-    _TRISA1 = 0;  
-    
-}
-void setupTimer(){
-    _TON = 1;       // Turn Timer1 on
-    _TCKPS = 0b01;  // Chose pre-scaling as 8
-    _TCS = 0;       // Internal clock source (FOSC/2)
-    TMR1 = 0;       // Reset Timer1
-}
-void setupQRDs(){
-    //SETUP THE ADC 
-     
-    _ADON = 0;    // Disable A/D module during configuration
-    
-    // AD1CON1
-    _MODE12 = 1;  // 12-bit resolution
-    _FORM = 0;    // unsigned integer output
-    _SSRC = 7;    // auto convert
-    _ASAM = 1;    // auto sample
-
-    // AD1CON2
-    _PVCFG = 0;   // use VDD as positive reference
-    _NVCFG = 0;   // use VSS as negative reference
-    _BUFREGEN = 1;// store results in buffer corresponding to channel number
-    _CSCNA = 1;   // scanning mode
-    _SMPI = 4;    // NUMBER OF ANALOG PINS MINUS ONE
-    _ALTS = 0;    // sample MUXA only
-
-    // AD1CON3
-    _ADRC = 0;    // use system clock
-    _SAMC = 1;    // sample every A/D period
-    _ADCS = 0x3F; // TAD = 64*TCY
-
-    //RIGHT
-    _TRISB2 = 1;
-    _ANSB2 = 1;
-    _CSS4 = 1;
-    
-    //MID
-    _TRISA2 = 1;
-    _ANSA2 = 1;
-    _CSS13 = 1;
-    
-    //LEFT
-    _TRISA3 = 1;
-    _ANSA3 = 1;
-    _CSS14 = 1;
-    
-    //TASKDETECTION
-    _TRISB12 = 1;
-    _ANSB12 = 1;
-    _CSS12 = 1;
-    
-    //SAMPLEDETECTION
-    _TRISB2 = 1;
-    _ANSB2 = 1;
-    _CSS4 = 0;
-    
-    //TURN ON ADC
-    _ADON = 1;
-    
 }
 
 //FSM FUNCTION DEFINITIONS
@@ -357,7 +275,24 @@ void taskDetectionFSM(){
             break;
         }
 }
-
+void canyonNavigationFSM(){
+    switch(canyonState){
+        case GOSTRAIGHT:
+            if(FRONTSENSOR){
+                canyonState = WALLDETECTED;
+                OC2Steps = 0;
+                tankTurn(90, CW);
+            }
+            break;
+            
+        case WALLDETECTED:
+            if(OC2Steps >= stepsToTake){
+                canyonState = GOSTRAIGHT;
+                driveStraight();
+            }
+            break;
+    }
+}
 //CONTROL FUNCTION DEFINITIONS
 void tankTurn(int degrees, int dir){
     
@@ -451,17 +386,27 @@ void hardLeft(){
 void search(){
     //TODO: Define this function
 }
+void debugLED(int onOff){
+    _LATB7 = onOff;
+}
+void openGate(){
+    OC1RS = SERVOPERIOD;
+    OC1R = OPENSERVO;
+}
+void closeGate(){
+    OC1RS = SERVOPERIOD;
+    OC1R = CLOSESERVO;
+}
 void sampleReturn(){
-    if(sampledetectionQRD()){
+    if(ballQRD()){
         tankTurn(90, CW);
         //TODO: Add code to open gate, close it, and then return to driving straight
     }
-    else if(!sampledetectionQRD()){
+    else if(!ballQRD()){
         tankTurn(90,CCW);
         //TODO: Add code to open gate, close it, and then return to driving straight
     }
 }
-
 //CHECK STATE FUNCTION DEFINITIONS
 int rightQRD(){
     int onOffr;
@@ -503,13 +448,13 @@ int taskdetectionQRD(){
     
     return onOfft;
 }
-int sampledetectionQRD(){
-    int onOffs;
-    if(ADC1BUF4 > THRESHOLD){
-        onOffs = 1;
+int ballQRD(){
+   int onOffb;
+    if(ADC1BUF11 > THRESHOLD){
+        onOffb = 1;
     }else{
-        onOffs = 0;
+        onOffb = 0;
     }
     
-    return onOffs;
+    return onOffb; 
 }
